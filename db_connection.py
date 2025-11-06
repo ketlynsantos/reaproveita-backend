3c939d6351f5f3be15135c3a81eb4027013fee28
Ketlyn Santos <ketlyn.santos@newtesc.com.br>
2025-11-06 19:49:52 -0300

    refactor: corrige formatação do arquivo db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,5 +1,6 @@
-import oracledb
 import os
+
+import oracledb
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,10 +9,13 @@ USER = os.getenv("DB_USER")
 PASSWORD = os.getenv("DB_PASSWORD")
 DSN = os.getenv("DB_DSN")
 
+
 def connect_to_db():
     try:
         if not USER or not PASSWORD or not DSN:
-            raise ValueError("Variáveis de ambiente DB_USER, DB_PASSWORD ou DB_DSN estão ausentes.")
+            raise ValueError(
+                "Variáveis de ambiente DB_USER, DB_PASSWORD ou DB_DSN estão ausentes."
+            )
 
         conn = oracledb.connect(
             user=USER,
